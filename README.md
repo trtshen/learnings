b39a18ef0eacf249cfa24e8fda67050fd7edacc7
chaw <cshen_581@hotmail.com>
2020-11-03 11:05:56 +0800

    show proper error

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-### Learnings
+### Learnings CORS
 
 Collections of any learning materials
 
 - Pusher https://github.com/trtshen/learnings/tree/pusher
 - Git Secret https://github.com/trtshen/learnings/tree/git-secret
 - SVG https://github.com/trtshen/learnings/tree/svg
+
+### How to use?
+
+1. run `npm start`
+1. in your browser, open developer console to look at the logs generated from the `app.js`.
--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 fetch('https://4d052q3ph6.execute-api.ap-southeast-2.amazonaws.com/notify', {
-	method: 'POST',
-	mode: 'cors',
-	headers: {
+  method: 'POST',
+  mode: 'cors',
+  headers: {
     'Content-Type': 'application/json',
-    // 'Content-Type': 'application/x-www-form-urlencoded',
     'origin': 'http://localhost:4200/'
   },
   body: JSON.stringify({})
 })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(err => {
-  	const result = document.getElementById('result');
-  	const childElement = document.createElement("p");
-  	childElement.innerHTML = JSON.stringify(err);
-  	result.appendChild(childElement);
-  });
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(err => {
+  const result = document.getElementById('result');
+  const childElement = document.createElement("p");
+  childElement.innerHTML = (typeof err === 'string') ? err : err.toString();
+  result.appendChild(childElement);
+});
