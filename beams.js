4f72938cadd0d6c0c304e7faa0e862c3e78fe575
chaw <cshen_581@hotmail.com>
2021-01-14 08:49:13 +0800

    simple template

--- beams.js
+++
@@ -1,9 +0,0 @@
-const PushNotifications = require('@pusher/push-notifications-server');
-const config = require('./config');
-
-const beamsClient = new PushNotifications({
-  instanceId: config.instanceId,
-  secretKey: config.secretKey,
-});
-
-module.exports = beamsClient;
